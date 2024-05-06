a729fc301ad0a29b6b3f4d3d7b5a5e100c608ac8
Barbara Samuel <barbara.samuel@yahoo.fr>
2024-05-06 11:57:16 +0200

    Base du projet de transactions

---
+++ src/test/java/org/paymybuddy/transfermoney/TransfermoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.paymybuddy.transfermoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransfermoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
